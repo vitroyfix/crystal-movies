526f351b59012e8cefb422c54947ffe04fc8bc63
peter wangimwa <186512834+vitroyfix@users.noreply.github.com>
2026-05-02 00:41:30 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -16,8 +16,8 @@ import { supabase } from "../../src/services/supabaseClient";
 import languages from "../data/langs.json";
 
 // ─── Constants ────────────────────────────────────────────────────────────────
-const BACKEND_URL = "https://birds-consciousness-spam-dolls.trycloudflare.com/api/fetch-stream";
-const SUBS_URL    = "https://birds-consciousness-spam-dolls.trycloudflare.com/api/subs";
+const BACKEND_URL = "https://moral-governing-extension-minister.trycloudflare.com/api/fetch-stream";
+const SUBS_URL    = "https://moral-governing-extension-minister.trycloudflare.com/api/subs";
 const SECRET_KEY  = import.meta.env.VITE_ENCRYPTION_KEY;
 const API_KEY     = import.meta.env.VITE_API_KEY;
 const TMDB_KEY    = import.meta.env.VITE_TMDB_API_KEY; 
