3c7e28499300980ca28a13ac19ccad8f9e0240af
vitroyfix <186512834+vitroyfix@users.noreply.github.com>
2026-05-12 19:51:04 +0300

    Updated tthe cloudfared urls

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -47,9 +47,9 @@ import languages from "../data/langs.json";
 
 // ─── Constants ────────────────────────────────────────────────────────────────
 const BACKEND_URL =
-  "https://females-sterling-thehun-cultural.trycloudflare.com/api/fetch-stream";
+  "https://yea-sculpture-experiment-graphs.trycloudflare.com/api/fetch-stream";
 const SUBS_URL =
-  "https://females-sterling-thehun-cultural.trycloudflare.com/api/subs";
+  "https://yea-sculpture-experiment-graphs.trycloudflare.com/api/subs";
 const API_KEY = import.meta.env.VITE_API_KEY;
 const TMDB_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const TMDB_BASE = "https://api.themoviedb.org/3";
