5edfac988d521c05e33c910d1fa20cdbc6d4a24c
vitroyfix <186512834+vitroyfix@users.noreply.github.com>
2026-05-13 15:26:54 +0300

    Updated tthe cloudfared urls

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -47,9 +47,9 @@ import languages from "../data/langs.json";
 
 // ─── Constants ────────────────────────────────────────────────────────────────
 const BACKEND_URL =
-  "https://highland-host-referenced-gardening.trycloudflare.com/api/fetch-stream";
+  "https://terminology-paintball-shareholders-from.trycloudflare.com/api/fetch-stream";
 const SUBS_URL =
-  "https://highland-host-referenced-gardening.trycloudflare.com/api/subs";
+  "https://terminology-paintball-shareholders-from.trycloudflare.com/api/subs";
 const API_KEY = import.meta.env.VITE_API_KEY;
 const TMDB_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const TMDB_BASE = "https://api.themoviedb.org/3";
