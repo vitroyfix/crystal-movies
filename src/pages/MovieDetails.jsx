a652ce65fa9bcd741026f4f9588c884462a4d96d
peter wangimwa <186512834+vitroyfix@users.noreply.github.com>
2026-06-11 22:43:39 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -46,8 +46,8 @@ import { supabase } from "../../src/services/supabaseClient";
 import languages from "../data/langs.json";
 
 // ─── Constants ────────────────────────────────────────────────────────────────
-const BACKEND_URL = "https://coming-faqs-deny-itunes.trycloudflare.com/api/fetch-stream";
-const SUBS_URL    = "https://coming-faqs-deny-itunes.trycloudflare.com/api/subs";
+const BACKEND_URL = "https://nuke-san-honors-photography.trycloudflare.com/api/fetch-stream";
+const SUBS_URL    = "https://nuke-san-honors-photography.trycloudflare.com/api/subs";
 const API_KEY = import.meta.env.VITE_API_KEY;
 const TMDB_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const TMDB_BASE = "https://api.themoviedb.org/3";
